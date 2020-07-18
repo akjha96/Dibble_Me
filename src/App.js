7aeb3762fd5fcc78ffad596f847dcc7d9f9f54d2
akjha96 <akjha96@gmail.com>
2020-07-18 15:24:04 +0530

    Finished Dibble me

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const handleCounter = (number) => {
+  return number * 2;
+};
+
+const App = () => {
+  // React Hooks
+  const [counter, setCounter] = useState(1);
+
+  let stomachFull = counter > 300;
+  if (stomachFull) {
+    alert("Private! I am full!!!");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Hello World!</header>
+      {/* Button to  be clicked */}
+      <div className="buttonStyle">
+        <div>
+          <button
+            onClick={() => {
+              setCounter(handleCounter(counter));
+            }}
+          >
+            Dibble me
+          </button>
+        </div>
+        <div className="labelStyle">
+          {/* Value to be displayed */}
+          <label>{counter}</label>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
